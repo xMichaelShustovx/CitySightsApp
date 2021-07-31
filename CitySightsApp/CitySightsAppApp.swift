aeca51413030acd39bcfbaa7bbc4c562eac93fd2
Michael Shustov <shustovmm@gmail.com>
2021-07-31 12:26:06 +0300

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Michael Shustov on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
